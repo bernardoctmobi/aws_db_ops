c50172a00dc56d5bd54e64d8e4c70709ab894725
Bernardo Di Miceli <bdm.444@gmail.com>
2023-07-25 17:16:23 +0200

    modificato handler e template yml

--- dbWrite/app.mjs
+++ dbWrite/app.mjs
@@ -1,17 +1,16 @@
-const AWS = require('aws-sdk');
-const dynamoDB = new AWS.dynamoDB.DocumentClient();
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const lambdaHandler = async (event) => {
     console.log(event);
-    
+
     try {
         const requestBody = JSON.parse(event.body);
-        await insertItem(requestBody);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Elemento inserito con successo' }),
-        }
+        const response = await insertItem(requestBody);
+        return response;
     } catch (err) {
         console.log(err);
         return {
@@ -22,7 +21,25 @@ export const lambdaHandler = async (event) => {
 };
 
 async function insertItem(itemData) {
-    const params = {
+    console.log(`siamo all'interno di insertItem`);
+    // const params = {
+    //     Key: {
+    //         userId: itemData.userId,
+    //     },
+    //     UpdateExpression: 'SET #name = :name, #surname = :surname',
+    //     ExpressionAttributeNames: {
+    //         '#name': 'name',
+    //         '#surname': 'surname',
+    //     },
+    //     ExpressionAttributeValues: {
+    //         ':name': itemData.name,
+    //         ':surname': itemData.surname,
+    //     },
+    //     TableName: process.env.DYNAMODB_TABLE,
+    //     Item: itemData,
+    // };
+
+    const command = new UpdateCommand({
         Key: {
             userId: itemData.userId,
         },
@@ -31,18 +48,22 @@ async function insertItem(itemData) {
             '#name': 'name',
             '#surname': 'surname',
         },
-        ExpressionAttributeValuse: {
+        ExpressionAttributeValues: {
             ':name': itemData.name,
             ':surname': itemData.surname,
         },
         TableName: process.env.DYNAMODB_TABLE,
         Item: itemData,
-    };
+    });
 
-    try {
-        await dynamoDB.update(params).promise();
-    } catch (error) {
-        console.error(`Errore durante l'aggiornamento dell'elemento:`, error);
-        throw error;
-    }
+    // try {
+    //     await DynamoDB.update(params).promise();
+    // } catch (error) {
+    //     console.error(`Errore durante l'aggiornamento dell'elemento:`, error);
+    //     throw error;
+    // }
+
+    const response = await docClient.send(command);
+    console.log(response);
+    return response;
 }
